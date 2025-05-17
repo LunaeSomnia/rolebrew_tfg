0ee3ecbfb4d7e93d16f66e28a3e8d6cf6809d314
LunaeSomnia <adrianmorales.t@proton.me>
2025-05-17 15:59:57 +0200

    fix: request from backed

--- frontend/src/routes/api/[...path]/+server.ts
+++ frontend/src/routes/api/[...path]/+server.ts
@@ -1,10 +1,22 @@
 import { PUBLIC_BACKEND_URL } from "$env/static/public";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = ({ params, url, fetch, request }) => {
-    return fetch(PUBLIC_BACKEND_URL + params.path + url.search, request);
+export const GET: RequestHandler = async ({ params, url, fetch, request }) => {
+    request.headers.append('Accept-Encoding', 'identity')
+    const fetchResult = await fetch(PUBLIC_BACKEND_URL + params.path + url.search, request)
+
+    const data = await fetchResult.json();
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' }
+    });
 };
 
-export const POST: RequestHandler = ({ params, url, fetch, request }) => {
-    return fetch(PUBLIC_BACKEND_URL + params.path + url.search, request);
+export const POST: RequestHandler = async ({ params, url, fetch, request }) => {
+    request.headers.append('Accept-Encoding', 'identity')
+    const fetchResult = await fetch(PUBLIC_BACKEND_URL + params.path + url.search, request)
+
+    const data = await fetchResult.json();
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' }
+    });
 };
